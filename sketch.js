fb1b20c31ed079938e93bdcc4a8e5a72a0e61921
~ Ananya ~ <63799268+Ananya2306@users.noreply.github.com>
2020-07-01 14:12:05 +0530

    Added Coments

--- sketch.js
+++ sketch.js
@@ -1,46 +1,60 @@
+// Add matter.js first
+// give the physics engines
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
-
+// create the array of ranindrops
 var raindrops = [];
+// make  a new umbrella 
 var img;
+// give image to the background
 var backgroundImg;
+// give image to thunder
 var thunder1,thunder2 ,thunderImg1 , thunderImg2 , thunderImg3;
 function preload(){
+  // load bg image
   backgroundImg = loadImage("Image/bg.jpg");
+  // load both thunder images
     thunder1 = loadImage("Image/i1 (1).png");
     thunder2 = loadImage("Image/i1 (2).png");
 }
 
 function setup(){
+  // create canvas
 var canvas = createCanvas(displayWidth - 20,displayHeight - 30);
+  // crate engine + world
     engine = Engine.create();
     world = engine.world;
+  // make sprites of thunderImages 1 , 2 , 3 
+  //  give positions to the sprites
 thunderImg1 = createSprite(displayWidth/2,5,10,40);
 thunderImg2 = createSprite(200,5,10,40);
 thunderImg3 = createSprite(1100,5,10,40);
-
+// give the positions to img of umbrella
     img = new Umbrella(displayWidth/2,500);
 }
 
 function draw(){
+  // add image to the background
     background(backgroundImg);
+  // update Engine
     Engine.update(engine);
-   
+   // create raindrops by frame count
     if(frameCount % 1 === 0){
       var raindrop = new Drop(random(0, windowWidth), -100,10);
       raindrops.push(raindrop);
     }
-
+// display umbrella img
     img.display();
-
+// display raindrops
     for(var i = 0; i < raindrops.length; i++){
       raindrops[i].fall(20);
       raindrops[i].display();
     } 
-
+// add random img to the thunder images 1
+  // By Using Switch case method
     if(frameCount % 10 === 0) {
     var rand = Math.round(random(1,2));
       switch(rand) {
@@ -51,6 +65,8 @@ function draw(){
         default: break;
       }
     }   
+  // add random img to the thunder images 2
+  // By Using Switch case method
     if(frameCount % 10 === 0) {
       var rand = Math.round(random(1,2));
         switch(rand) {
@@ -61,6 +77,8 @@ function draw(){
           default: break;
         }
       }   
+  // add random img to the thunder images 3
+  // By Using Switch case method
       if(frameCount % 10 === 0) {
         var rand = Math.round(random(1,2));
           switch(rand) {
@@ -71,6 +89,7 @@ function draw(){
             default: break;
           }
         } 
+  // for drawing the sprites write drawSprites at the end 
     drawSprites();
 }   
 
