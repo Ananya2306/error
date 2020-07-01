e99604037fed27c221cf6f06dbb6909345d4b3c1
~ Ananya ~ <63799268+Ananya2306@users.noreply.github.com>
2020-07-01 13:59:23 +0530

    Added comment

--- Js/Drop.js
+++ Js/Drop.js
@@ -1,14 +1,16 @@
 class Drop{
     constructor(x,y,radius){
-        this.body = ellipse(x,y,radius)
+        //  shape of the body
+        this.body = ellipse(x,y,radius);
         this.radius = radius;
         this.x = x;
         this.y = y;
     }
+    //make fall function to add speed 
     fall(speed){
         this.y +=speed;
     }
-  
+  // display function to guve the x,y,radius and color
     display(){
         push();
         fill("silver");
