263394331404d65e6b204b9c617724111fa2f9c2
~ Ananya ~ <63799268+Ananya2306@users.noreply.github.com>
2020-07-01 14:04:01 +0530

    Added Comments

--- Js/Umbrella.js
+++ Js/Umbrella.js
@@ -1,16 +1,18 @@
 class Umbrella{
     constructor(x,y){
 var options ={
+    // giveisStaic true 
     isStatic : true
 }
     
-
+// give shape of the body and add image to it
     this.body = Bodies.rectangle(x, y, width,height, options);
     this.width = width;
     this.height = height;
     this.image = loadImage("Image/img.png");
     World.add(world, this.body);
 }
+    // make display function to give the height and width and angle of the body + image
 display(){
     var angle = this.body.angle;
     push();
